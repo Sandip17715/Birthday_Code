2e9a03ba3740b02cef639b44e482d22b7be6c9ba
Sandip Maity <119385562+Sandip17715@users.noreply.github.com>
2022-11-29 09:27:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var a = prompt("Enter UserName");
+var b = prompt("Enter password");
+if (a == "Sandip" && b == "17.07.15") {
+  window.location.href = "start.html";
+} else {
+  alert("invalid details");
+  window.location.href = "index.html";
+}
